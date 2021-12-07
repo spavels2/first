bd4bd9bf09d6604f4c94af398b816f6e136b56b8
spavels2 <gloryz123@yandex.ru>
2021-12-07 20:23:28 +0800

    add file App.js App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://e7.pngegg.com/pngimages/539/883/png-clipart-globe-earth-globe-miscellaneous-blue.png" />
       </header>
+      <nav className='nav'>
+        <div>
+         <a>Profile</a>
+        </div>
+        <div>
+         <a>Messages</a>
+        </div>
+        <div>
+         <a>News</a>
+        </div>
+        <div>
+         <a>Music</a>
+        </div>
+        <div>
+         <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src="https://www.silverskytravel.co.tz/images/holiday-in-zanzibar-3-days-banner.jpg" alt="#"/>
+        <img className='content_logo' src="https://sun9-48.userapi.com/s/v1/if1/dwXmvoTlRzz_z7_K2PpTecyBP4GBsFARWyJy7iyWPO5fCSsbJhmmp-YK7aXTgJ21JuSO3g.jpg?size=100x0&quality=96&crop=37,53,213,213&ava=1" alt="#"/>
+        <div>ava+description</div>
+        <div>My post</div>
+        <div>New post</div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
     </div>
   );
 }
